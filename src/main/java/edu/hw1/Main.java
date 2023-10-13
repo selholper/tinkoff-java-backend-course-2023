810d6a794714dc82ab33f5b4c37c1f380e69ac63
Ivan <selholper@gmail.com>
2023-10-13 19:06:35 +0300

    Initial commit

---
+++ src/main/java/edu/hw1/Main.java
@@ -0,0 +1,15 @@
+package edu.hw1;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public final class Main {
+    private final static Logger LOGGER = LogManager.getLogger();
+
+    private Main() {
+    }
+
+    public static void main(String[] args) {
+        LOGGER.info("Привет, мир!");
+    }
+}
