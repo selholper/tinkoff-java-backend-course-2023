bbf2006dcad4cc3c442d2db5cefd9b3f440b107b
Ivan <selholper@gmail.com>
2023-10-22 23:06:28 +0300

    Add coverage test

--- src/test/java/edu/project1/TestInMemoryConfiguration.java
+++ src/test/java/edu/project1/TestInMemoryConfiguration.java
@@ -6,7 +6,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 public class TestInMemoryConfiguration {
     @Test
-    void testInMemoryConfiguration() {
+    void testInMemoryConfigurationConstructor() {
         assertThatThrownBy(
             () -> new InMemoryConfiguration(-1)
         ).isInstanceOf(IllegalArgumentException.class);
@@ -17,6 +17,28 @@ public class TestInMemoryConfiguration {
 
         assertDoesNotThrow(
             () -> new InMemoryConfiguration(1)
+        );
+
+        assertDoesNotThrow(
+            () -> new InMemoryConfiguration()
+        );
+    }
+
+    @Test
+    void testInMemoryConfigurationGetMaxAttemptsNumber() {
+        InMemoryConfiguration inMemoryConfiguration = new InMemoryConfiguration();
+
+        assertDoesNotThrow(
+            inMemoryConfiguration::getMaxAttemptsNumber
+        );
+    }
+
+    @Test
+    void testInMemoryConfigurationGetDictionary() {
+        InMemoryConfiguration inMemoryConfiguration = new InMemoryConfiguration();
+
+        assertDoesNotThrow(
+            inMemoryConfiguration::getDictionary
         );
     }
 
