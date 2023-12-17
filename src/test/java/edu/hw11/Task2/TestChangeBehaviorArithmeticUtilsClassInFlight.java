501a2cb3632bc86dcae811723eb4d6e4bc562919
Ivan <selholper@gmail.com>
2023-12-17 19:59:52 +0300

    Add more tests for task2

--- src/test/java/edu/hw11/Task2/TestChangeBehaviorArithmeticUtilsClassInFlight.java
+++ src/test/java/edu/hw11/Task2/TestChangeBehaviorArithmeticUtilsClassInFlight.java
@@ -1,14 +1,26 @@
 package edu.hw11.Task2;
 
+import java.util.stream.Stream;
 import net.bytebuddy.agent.ByteBuddyAgent;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class TestChangeBehaviorArithmeticUtilsClassInFlight {
-    @Test
-    public void testReloadM_shouldChangeSumMethodInFlight() {
+    private static Stream<Arguments> testReloadM_shouldChangeSumMethodInFlight() {
+        return Stream.of(
+            Arguments.of(1, 2, 2),
+            Arguments.of(2, 3, 6),
+            Arguments.of(11, 17, 187)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    public void testReloadM_shouldChangeSumMethodInFlight(int a, int b, int res) {
         ByteBuddyAgent.install();
         ClassBehaviorReloader.reload();
-        Assertions.assertThat(ArithmeticUtils.sum(11, 17)).isEqualTo(187);
+        Assertions.assertThat(ArithmeticUtils.sum(a, b)).isEqualTo(res);
     }
 }
