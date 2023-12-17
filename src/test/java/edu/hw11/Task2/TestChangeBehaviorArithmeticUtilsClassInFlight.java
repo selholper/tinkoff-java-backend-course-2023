5141afbd98d2743def1ac2319338ffceab4ac5f6
Ivan <selholper@gmail.com>
2023-12-17 19:51:02 +0300

    Minor fixes

--- src/test/java/edu/hw11/Task2/TestChangeBehaviorArithmeticUtilsClassInFlight.java
+++ src/test/java/edu/hw11/Task2/TestChangeBehaviorArithmeticUtilsClassInFlight.java
@@ -1,26 +1,14 @@
 package edu.hw11.Task2;
 
-import java.util.stream.Stream;
 import net.bytebuddy.agent.ByteBuddyAgent;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.Test;
 
 public class TestChangeBehaviorArithmeticUtilsClassInFlight {
-    private static Stream<Arguments> testReloadM_shouldChangeSumMethodInFlight() {
-        return Stream.of(
-            Arguments.of(1, 2, 2),
-            Arguments.of(2, 3, 6),
-            Arguments.of(11, 4999, 54989)
-        );
-    }
-
-    @ParameterizedTest
-    @MethodSource
-    public void testReloadM_shouldChangeSumMethodInFlight(int a, int b, int c) {
+    @Test
+    public void testReloadM_shouldChangeSumMethodInFlight() {
         ByteBuddyAgent.install();
         ClassBehaviorReloader.reload();
-        Assertions.assertThat(ArithmeticUtils.sum(a, b)).isEqualTo(c);
+        Assertions.assertThat(ArithmeticUtils.sum(11, 4999)).isEqualTo(54989);
     }
 }
